85b83736e73f0d942a01a455dc42a3d0b2096665
nda97531 <42064053+nda97531@users.noreply.github.com>
2019-01-05 20:45:39 +0700

    Update sign_classi.py

--- src/sign_classi.py
+++ src/sign_classi.py
@@ -1,20 +1,20 @@
 import torch as tr, cv2, numpy as np
-device = tr.device("cuda:0" if tr.cuda.is_available() else "cpu")
+device = tr.device("cuda" if tr.cuda.is_available() else "cpu")
 
 class CNN32(tr.nn.Module):
     def __init__(self):
         super(CNN32, self).__init__() #3x32x32
         self.pool = tr.nn.MaxPool2d(2, 2)
 
-        self.conv1 = tr.nn.Conv2d(3, 32, 5) #28 (in_channels=3, out_channels=64, kernel_size=5, stride=1, padding=0)
+        self.conv1 = tr.nn.Conv2d(3, 16, 5) #28 (in_channels=3, out_channels=64, kernel_size=5, stride=1, padding=0)
         #pool 14
-        self.conv2 = tr.nn.Conv2d(32, 64,3) #12
+        self.conv2 = tr.nn.Conv2d(16, 32,3) #12
         #pool 6
-        self.conv3 = tr.nn.Conv2d(64, 128, 3) #4
+        self.conv3 = tr.nn.Conv2d(32, 64, 3) #4
         #pool 2
-        self.fc1 = tr.nn.Linear(128 *2 *2, 1024) #
-        self.fc2 = tr.nn.Linear(1024, 512)
-        self.fc3 = tr.nn.Linear(512, 3)
+        self.fc1 = tr.nn.Linear(64 *2 *2, 128) #
+        self.fc2 = tr.nn.Linear(128, 64)
+        self.fc3 = tr.nn.Linear(64, 3)
 
 
     def forward(self, X): #3*32*32 #3*227*227
@@ -33,11 +33,13 @@ class CNN32(tr.nn.Module):
         return X
 
 net= CNN32().to(device)
-net.load_state_dict(tr.load('param/sign_classi_param_9932_half', map_location=device))
+net.load_state_dict(tr.load('../param/sign_classi_param32_small'))
 
-def predict(img, new_size=32):
-    img = np.array(img, dtype= np.float32) / 255.
+def predict(img):
+    new_size = 32
     img = cv2.resize(img, (new_size, new_size))
+    if img.max()>1:
+        img = np.array(img, dtype= np.float32) / 255.
 
     img= img.reshape(1,new_size,new_size,3).transpose((0,3,1,2))
 
@@ -47,15 +49,3 @@ def predict(img, new_size=32):
         output= tr.argmax(output)
 
     return int(output) #0= not turn; 1= turn right, 2= turn left
-
-# with tr.no_grad():
-#     # while True:
-#     #     dir= raw_input("file directory: ")
-#     #     if dir == '': break
-#     for i in range(24,28):
-#         dir= 'other imgs/o27.png'#'other imgs/o' + str(i) + '.png'
-#
-#         img= cv2.imread(dir)
-#         img= np.flip(img,1)
-#         cv2.imshow(str(predict(img)), cv2.resize(img, (150,150)))
-#         cv2.waitKey(0)
